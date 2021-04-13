088e5b79d2c32204447e64a9ea2c500abb7360d5
khawarzia-oslab <80633412+khawarzia-oslab@users.noreply.github.com>
2021-04-13 13:52:33 +0500

    lab3 shell 2017ee48

--- work.c
+++ work.c
@@ -14,6 +14,33 @@
 *
 */
 
+int get_non_zero_data(char *line){
+	int i;
+	for (i = 0;i < strlen(line); i++){
+		if (line[i] == '0'){
+			break;
+		}
+	}
+	i = i-2;
+	return i;
+}
+
+int check_for_amp_sign(char *line){
+	int i,out=0;
+	for (i = 0; i < strlen(line); i++){
+		if (line[i] == '&'){
+			if (line[++i] == '&'){
+				out = 1;
+				if (line[++i] == '&'){
+					out = 2;
+					break;
+				}
+			}
+		}
+	}
+	return out;
+}
+
 
 char **tokenize(char *line)
 {
@@ -36,7 +63,9 @@ char **tokenize(char *line)
 	  strcpy(char1, "/bin/");
 	}
 	else{
-	  strcpy(tokens[tokenNo++], token);
+	  if (strcmp(token,"&") != 0 && strcmp(token,"&&") != 0 && strcmp(token,"&&&") != 0){
+	    strcpy(tokens[tokenNo++], token);
+	  }
 	}
 	//printf("this %s ",tokens[tokenNo-1]);
 	tokenIndex = 0; 
@@ -54,10 +83,11 @@ char **tokenize(char *line)
 
 
 int main(int argc, char *argv[]) {
-	char  line[MAX_INPUT_SIZE],temp_line[MAX_INPUT_SIZE];            
+	char  line[MAX_INPUT_SIZE],temp_line[MAX_INPUT_SIZE],temp_line2[MAX_INPUT_SIZE];          
 	char  **tokens;              
-	int i,output;
+	int i,output,func_check=0;
 	char cwd[256];
+	char *token2;
 
 	FILE* fp;
 	FILE* hist;
@@ -84,56 +114,199 @@ int main(int argc, char *argv[]) {
 			printf(" $ ");
 			scanf("%[^\n]", line);
 			getchar();
-			printf("%ld",strlen(line));
 		}
-		//printf("Command entered: %s (remove this debug output later)\n", line);
+
 		/* END: TAKING INPUT */
 
-		
-		if (strcmp(line,"!!") == 0){
-			hist = fopen(".history.txt","r");
-			if (hist == NULL){
-				printf("No commands in history\n");
-				continue;
-			}
-			fgets(line,MAX_INPUT_SIZE,hist);
-			fclose(hist);
-			printf("%s\n",line);
-		}
-		else{
-			hist = fopen(".history.txt","w");
-			fprintf(hist,"%s",line);
-			fclose(hist);
-		}
-		strcpy(temp_line,line);
-		line[strlen(line)] = '\n'; //terminate with new line
-		tokens = tokenize(line);
-   
-       		//do whatever you want with the commands, here we just print them
+		func_check = check_for_amp_sign(line);
 
-		for(i=0;tokens[i]!=NULL;i++){
-			//printf("found token %s (remove this debug output later)\n", tokens[i]);
-		}
 
-		if (strcmp(tokens[0],"/bin/cd") == 0){
-			chdir(tokens[1]);
-		}
-		else{
-			if (fork() == 0){
-				output = execv(tokens[0],tokens);
-				if (output == -1){
-					printf("%s is not a recognized command\n",temp_line);			
-				}
-				else{
-					printf("\n");
-				}
+		if (func_check == 0){
+			if (strcmp(line,"exit") == 0){
+				kill(0,0);
 				exit(0);
 			}
+			
+			if (strcmp(line,"!!") == 0){
+				hist = fopen(".history.txt","r");
+				if (hist == NULL){
+					printf("No commands in history\n");
+					continue;
+				}
+				fgets(line,MAX_INPUT_SIZE,hist);
+				fclose(hist);
+				printf("%s\n",line);
+			}
 			else{
-				wait(NULL);
-	       		}
+				hist = fopen(".history.txt","w");
+				fprintf(hist,"%s",line);
+				fclose(hist);
+			}
+
+			strcpy(temp_line,line);
+			line[strlen(line)] = '\n'; //terminate with new line
+			tokens = tokenize(line);
+
+			if (line[get_non_zero_data(line)] == '&'){
+				//printf("& check\n");
+				if (fork() == 0){
+					output = execv(tokens[0],tokens);
+					if (output == -1){
+						printf("%s is not a recognized command\n",temp_line);			
+					}
+					else{
+						printf("\n");
+					}
+					exit(0);
+				}
+				continue;
+			}
+	   
+	       		//do whatever you want with the commands, here we just print them
+
+			for(i=0;tokens[i]!=NULL;i++){
+				//printf("found token %s (remove this debug output later)\n", tokens[i]);
+			}
+
+			if (strcmp(tokens[0],"/bin/cd") == 0){
+				chdir(tokens[1]);
+			}
+			else{
+				if (fork() == 0){
+					output = execv(tokens[0],tokens);
+					if (output == -1){
+						printf("%s is not a recognized command\n",temp_line);			
+					}
+					else{
+						printf("\n");
+					}
+					exit(0);
+				}
+				else{
+					wait(NULL);
+		       		}
+			}
 		}
 
+
+		else if (func_check == 1){
+			token2 = strtok(line,"&&");
+			while (token2 != NULL){
+				if (strcmp(token2,"exit") == 0){
+					kill(0,0);
+					exit(0);
+				}
+				
+				if (strcmp(token2,"!!") == 0){
+					hist = fopen(".history.txt","r");
+					if (hist == NULL){
+						printf("No commands in history\n");
+						continue;
+					}
+					fgets(token2,MAX_INPUT_SIZE,hist);
+					fclose(hist);
+					printf("%s\n",token2);
+				}
+				else{
+					hist = fopen(".history.txt","w");
+					fprintf(hist,"%s",token2);
+					fclose(hist);
+				}
+
+				strcpy(temp_line,token2);
+				token2[strlen(token2)] = '\n'; //terminate with new token2
+				tokens = tokenize(token2);
+		   
+		       		//do whatever you want with the commands, here we just print them
+
+				for(i=0;tokens[i]!=NULL;i++){
+					//printf("found token %s (remove this debug output later)\n", tokens[i]);
+				}
+
+				if (strcmp(tokens[0],"/bin/cd") == 0){
+					chdir(tokens[1]);
+				}
+				else{
+					if (fork() == 0){
+						output = execv(tokens[0],tokens);
+						if (output == -1){
+							printf("%s is not a recognized command\n",temp_line);			
+						}
+						else{
+							printf("\n");
+						}
+						exit(0);
+					}
+					else{
+						wait(NULL);
+			       		}
+				}
+				token2 = strtok(NULL,"&&");
+				if (strcmp(token2,"\n") == 0){
+					break;
+				}
+			}
+			continue;
+		}
+
+
+		else {
+			token2 = strtok(line,"&&&");
+			while (token2 != NULL){
+				printf("%s\n",token2);
+				if (strcmp(token2,"exit") == 0){
+					kill(0,0);
+					exit(0);
+				}
+				
+				if (strcmp(token2,"!!") == 0){
+					hist = fopen(".history.txt","r");
+					if (hist == NULL){
+						printf("No commands in history\n");
+						continue;
+					}
+					fgets(token2,MAX_INPUT_SIZE,hist);
+					fclose(hist);
+					printf("%s\n",token2);
+				}
+				else{
+					hist = fopen(".history.txt","w");
+					fprintf(hist,"%s",token2);
+					fclose(hist);
+				}
+
+				strcpy(temp_line,token2);
+				token2[strlen(token2)] = '\n'; //terminate with new token2
+				tokens = tokenize(token2);
+		   
+		       		//do whatever you want with the commands, here we just print them
+
+				for(i=0;tokens[i]!=NULL;i++){
+					//printf("found token %s (remove this debug output later)\n", tokens[i]);
+				}
+
+				if (strcmp(tokens[0],"/bin/cd") == 0){
+					chdir(tokens[1]);
+				}
+				else{
+					if (fork() == 0){
+						output = execv(tokens[0],tokens);
+						if (output == -1){
+							printf("%s is not a recognized command\n",temp_line);			
+						}
+						else{
+							printf("\n");
+						}
+						exit(0);
+					}
+				}
+				token2 = strtok(NULL,"&&&");
+				if (strcmp(token2,"\n") == 0){
+					break;
+				}
+			}
+			continue;
+		}
 		
 		// Freeing the allocated memory	
 		for(i=0;tokens[i]!=NULL;i++){
